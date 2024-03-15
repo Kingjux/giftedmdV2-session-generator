039d643d2bce93616f38cd699526e2353831a272
giftedtech <admin@giftedtechnexus.co.ke>
2024-03-15 05:38:13 +0300

    ɢɪғᴛᴇᴅ ᴛᴇᴄʜ
    ɢɪғᴛᴇᴅ ᴛᴇᴄʜ

--- qr.js
+++ qr.js
@@ -1,90 +1,129 @@
-const PastebinAPI = require('pastebin-js'),
-pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
-const {makeid} = require('./id');
-const QRCode = require('qrcode');
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-let router = express.Router()
+
+const express = require("express");
+const app = express();
+
+
 const pino = require("pino");
-const {
-	default: Gifted_Tech,
-	useMultiFileAuthState,
-	jidNormalizedUser,
-	Browsers,
-	delay,
-	makeInMemoryStore,
-} = require("@whiskeysockets/baileys");
+let { toBuffer } = require("qrcode");
+const path = require('path');
+const fs = require("fs-extra");
+const { Boom } = require("@hapi/boom");
+const PORT = process.env.PORT ||  5000
+const MESSAGE = process.env.MESSAGE ||  `
+╔════◇
+║ *『 WOW YOU'VE CHOSEN GIFTED-MDv2 』*
+║ _You Have Completed the First Step to Deploy a Whatsapp Bot._
+╚════════════════════════╝
+╔═════◇
+║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
+║❒ *Ytube:* _youtube.com/@giftedtechnexus_
+║❒ *Owner:* _https://wa.me/message/NHCZC5DSOEUXB1_
+║❒ *Repo:* _https://github.com/Giftedmaurice/gifted-mdV2_
+║❒ *WaGroup:* _https://chat.whatsapp.com/L0ctUUVLlsrFYwBHApKfew_
+║❒ *WaChannel:* _https://whatsapp.com/channel/0029VaJmfmTDJ6H7CmuBss0o_
+║❒ *Plugins:* _https://github.com/Giftedmaurice/gifted-bot-md-plugins_
+╚════════════════════════╝
 
-function removeFile(FilePath) {
-	if (!fs.existsSync(FilePath)) return false;
-	fs.rmSync(FilePath, {
-		recursive: true,
-		force: true
-	})
-};
-const {
-	readFile
-} = require("node:fs/promises")
-router.get('/', async (req, res) => {
-	const id = makeid();
-	async function GIFTED_MD_QR_CODE() {
-		const {
-			state,
-			saveCreds
-		} = await useMultiFileAuthState('./temp/' + id)
-		try {
-			let Qr_Code_By_Gifted_Tech = Gifted_Tech({
-				auth: state,
-				printQRInTerminal: false,
-				logger: pino({
-					level: "silent"
-				}),
-				browser: Browsers.macOS("Desktop"),
-			});
-
-			Qr_Code_By_Maher_Zubair.ev.on('creds.update', saveCreds)
-			Qr_Code_By_Maher_Zubair.ev.on("connection.update", async (s) => {
-				const {
-					connection,
-					lastDisconnect,
-					qr
-				} = s;
-				if (qr) await res.end(await QRCode.toBuffer(qr));
-				if (connection == "open") {
-					await delay(5000);
-					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
-					await delay(800);
-				   let b64data = Buffer.from(data).toString('base64');
-				   let session = await Qr_Code_By_Gifted_Tech.sendMessage(Qr_Code_By_Gifted_Tech.user.id, { text: 'GIFTED-MD;;;' + b64data });
-	
-				   let GIFTED_MD_TEXT = `
-*_Qr Code By Gifted Tech_*
-*_Made With 🤍_*
-	
-_Don't Forget To Give Star To My Repo_`
-	 await Qr_Code_By_Gifted_Tech.sendMessage(Qr_Code_By_Gifted_Tech.user.id,{text:GIFTED_MD_TEXT},{quoted:session})
+`
 
 
 
-					await delay(100);
-					await Qr_Code_By_Gifted_Tech.ws.close();
-					return await removeFile("temp/" + id);
-				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
-					await delay(10000);
-					GIFTED_MD_QR_CODE();
-				}
-			});
-		} catch (err) {
-			if (!res.headersSent) {
-				await res.json({
-					code: "Service Unavailable"
-				});
-			}
-			console.log(err);
-			await removeFile("temp/" + id);
-		}
-	}
-	return await GIFTED_MD_QR_CODE()
+if (fs.existsSync('./auth_info_baileys')) {
+    fs.emptyDirSync(__dirname + '/auth_info_baileys');
+  };
+  
+  app.use("/", async(req, res) => {
+
+  const { default: GiftedWASocket, useMultiFileAuthState, Browsers, delay,DisconnectReason, makeInMemoryStore, } = require("@whiskeysockets/baileys");
+  const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) })
+  async function GIFTED() {
+    const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys')
+    try {
+      let Smd =GiftedWASocket({ 
+        printQRInTerminal: false,
+        logger: pino({ level: "silent" }), 
+        browser: Browsers.baileys("Desktop"),
+        auth: state 
+        });
+
+
+      Smd.ev.on("connection.update", async (s) => {
+        const { connection, lastDisconnect, qr } = s;
+        if (qr) { res.end(await toBuffer(qr)); }
+
+
+        if (connection == "open"){
+          await delay(3000);
+          let user = Smd.user.id;
+
+
+//===========================================================================================
+//===============================  SESSION ID    ===========================================
+//===========================================================================================
+
+          let CREDS = fs.readFileSync(__dirname + '/auth_info_baileys/creds.json')
+          var Scan_Id = Buffer.from(CREDS).toString('base64')
+         // res.json({status:true,Scan_Id })
+          console.log(`
+====================  SESSION ID  ==========================                   
+SESSION-ID ==> ${Scan_Id}
+-------------------   SESSION CLOSED   -----------------------
+`)
+
+
+          let msgsss = await Smd.sendMessage(user, { text:  Scan_Id });
+          await Smd.sendMessage(user, { text: MESSAGE } , { quoted : msgsss });
+          await delay(1000);
+          try{ await fs.emptyDirSync(__dirname+'/auth_info_baileys'); }catch(e){}
+
+
+        }
+
+        Smd.ev.on('creds.update', saveCreds)
+
+        if (connection === "close") {            
+            let reason = new Boom(lastDisconnect?.error)?.output.statusCode
+            // console.log("Reason : ",DisconnectReason[reason])
+            if (reason === DisconnectReason.connectionClosed) {
+              console.log("Connection closed!")
+             // GIFTED().catch(err => console.log(err));
+            } else if (reason === DisconnectReason.connectionLost) {
+                console.log("Connection Lost from Server!")
+            //  GIFTEDL().catch(err => console.log(err));
+            } else if (reason === DisconnectReason.restartRequired) {
+                console.log("Restart Required, Restarting...")
+              GIFTED().catch(err => console.log(err));
+            } else if (reason === DisconnectReason.timedOut) {
+                console.log("Connection TimedOut!")
+             // GIFTED().catch(err => console.log(err));
+            }  else {
+                console.log('Connection closed with bot. Please run again.');
+                console.log(reason)
+              //process.exit(0)
+            }
+          }
+
+
+
+      });
+    } catch (err) {
+        console.log(err);
+       await fs.emptyDirSync(__dirname+'/auth_info_baileys'); 
+    }
+  }
+
+
+  GIFTED().catch(async(err) => {
+    console.log(err)
+    await fs.emptyDirSync(__dirname+'/auth_info_baileys'); 
+
+
+    //// MADE WITH GIFTED TECH
+
 });
-module.exports = router
+
+
+  })
+
+
+app.listen(PORT, () => console.log(`App listened on port http://localhost:${PORT}`));
